b93e56547cd880cffe60e38156833afffc8cfef6
SrinivasanJayakumar75 <sjayakumar862412@gmail.com>
2025-11-16 21:58:31 +0530

    09:widget-layout

---
+++ apps/widgets/modules/widget/ui/views/widget-view.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { WidgetFooter } from "../components/widget-footer";
+import { WidgetHeader } from "../components/widget-header";
+
+interface Props {
+    organizationId: string;
+};
+
+export const WidgetView = ({ organizationId }: Props) => {
+    return (
+        <main className="min-h-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
+            <WidgetHeader>
+                <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
+                    <p className="font-semibold text-3xl">
+                        Hi there! ✋
+                    </p>
+                    <p className="text-lg">
+                        How can we help you today?
+                    </p>
+
+                </div>
+            </WidgetHeader>
+            Widget View: {organizationId}
+            <WidgetFooter/>
+        </main>
+    )
+
+}
