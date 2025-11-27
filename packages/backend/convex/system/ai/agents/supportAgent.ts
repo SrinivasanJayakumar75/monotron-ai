a97924e198198f6e5ffeaceb7dc7a5f8364f51c6
SrinivasanJayakumar75 <sjayakumar862412@gmail.com>
2025-11-27 22:18:38 +0530

    19:ai tool calling

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -2,8 +2,10 @@ import { google } from '@ai-sdk/google';
 import { Agent } from "@convex-dev/agent";
 import { components} from "../../../_generated/api";
 
+
 export const supportAgent = new Agent(components.agent, {
     name: "Support Agent",
     chat: google("gemini-2.5-flash"),
-    instructions: "You are a helpful assistant",    
+    instructions: `You are a customer support agent. Use "resolveConversation" tool when user expresses finalization of the conversation. Use "escalateConversation" tool when user expresses frustration, or requests a human explicitly.`,
+    
 })
