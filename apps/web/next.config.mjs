8942bd0b8771b3be75429324b1a2bdcbff00d9e3
SrinivasanJayakumar75 <sjayakumar862412@gmail.com>
2025-12-22 21:17:32 +0530

    de

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  transpilePackages: ["@workspace/ui", "@workspace/backend"],
+  
+  transpilePackages: ["@workspace/ui"],
 }
 
 export default nextConfig
