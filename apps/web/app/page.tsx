0fcfd644787ce3d110b7ac8d9e5810e9dd8e5816
SrinivasanJayakumar75 <sjayakumar862412@gmail.com>
2025-12-23 22:42:20 +0530

    nox

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,9 @@
-import { Header } from "./sections/header"
 import {Hero} from "./sections/hero"
-import { LogoTicker } from "./sections/logoTicker"
 import { ProductShowcase } from "./sections/productShowcase"
 import { Footer } from "./sections/footer"
 import FeaturesSection from "./sections/featureSection"
 import FeatureIn from "./sections/FeatureIn"
-import Script from "next/script";
+
 
 export default function Page  () {
     return (
